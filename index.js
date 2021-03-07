67144c4b793d39134a39da82519adddd942d4831
Piyachat Mongkolpatthanakul <taeaew.w@outlook.com>
2021-03-07 13:24:12 +0700

    add product list api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const json = require("koa-json");
+const cors = require("@koa/cors");
+
+const products = require("./data/products.json");
 
 const app = new Koa();
 const router = new Router();
@@ -8,9 +11,21 @@ const router = new Router();
 const PORT = 8000;
 
 app.use(json());
+app.use(
+  cors({
+    origin: "*",
+    allowedMethods: ["GET", "PUT", "POST", "DELETE"],
+    exposeHeaders: ["X-Request-Id"],
+  })
+);
 
-router.get("/test", async (ctx) => {
-  ctx.body = 'Hello';
+router.get("/products/:locationId", async (ctx) => {
+  const { locationId = "" } = ctx.params;
+  ctx.body = products[locationId];
+});
+
+router.get("/locations/options", async (ctx) => {
+  ctx.body = Object.keys(products);
 });
 
 app.use(router.routes()).use(router.allowedMethods());
